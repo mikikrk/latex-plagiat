72455984af66b0bcd4322c056bf306f66f05e09e
miki_n <miki_n@bitbucet.org>
2016-06-02 22:19:27 +0200

    Added latex replacements

--- server/src/main/java/com/zpi/plagiarism_detector/server/articles/TextProcessing.java
+++ server/src/main/java/com/zpi/plagiarism_detector/server/articles/TextProcessing.java
@@ -11,11 +11,23 @@ import com.zpi.plagiarism_detector.server.ServerProperties;
 
 public class TextProcessing {
 
-    private String patternPath;
-    private String textPath;
     private double sentenceWordsCount;
     private double repeatedWords;
     private Map<PlagiarismFragment, PlagiarismFragment> map;
+
+	private static final String[] latexReplacements = {
+			"\\\\multicolumn{",
+			"\\\\caption{",
+			"\\\\chapter{",
+			"\\\\subsection{",
+			"\\\\section{",
+			"\\\\mbox{",
+			"\\\\emph{",
+			"\\\\section{",
+			"\\\\text[^{]*{",
+			"\\\\title{",
+			"\\\\[^ \\n]+"
+	};
     
     public TextProcessing() {
     }
@@ -24,40 +36,57 @@ public class TextProcessing {
      * W�asciwe por�wnanie
      *
      * @param str
-     * @param p
+     * @param pattern
      * @param patternStart
      */
-    public Map<PlagiarismFragment, PlagiarismFragment> compareTexts(String[] str, String p,int patternStart) {
-    	map  = new LinkedHashMap<PlagiarismFragment, PlagiarismFragment>();
+    public Map<PlagiarismFragment, PlagiarismFragment> compareTexts(String[] str, String pattern,int patternStart) {
+    	map  = new LinkedHashMap<>();
         int textStart = 0;
-        String tmpPattern = p;
+        String tmpPattern = pattern;
         for(String textSentence : str){
     		repeatedWords=0;
     		String tmpText = textSentence;
-        	String result = textSentence.replaceAll("[()-+.^:,]","");
-        	result=result.replaceAll("\\b[\\w']{1,2}\\b", "");
-        	result = result.replaceAll("\\s{2,}", " ");
-        	String[] splited = result.split("\\s+");
+        	String[] splited =  prepareText(textSentence);
         	sentenceWordsCount=splited.length-1;
-        	p=p.replaceAll("[()-+.^:,]","");
-        	p = p.replaceAll("\\b[\\w']{1,2}\\b", "");
-            p = p.replaceAll("\\s{2,}", " ");
-        	String[] splitedPattern = p.split("\\s+");
-        	for(String test: splitedPattern) {
-        		if(textSentence.toLowerCase().contains(test.toLowerCase())){
-        			repeatedWords++;
-        		}   		
-        	}
-        	
-    		repeatedWords-=1;  
+			if (sentenceWordsCount > 3) {
 
-    		if(repeatedWords/sentenceWordsCount>ServerProperties.MIN_WORD_SIMILARITY_PERCENTAGE/(double)100){
-    			 map.put(new PlagiarismFragment(tmpPattern, patternStart, patternStart+tmpPattern.length(), tmpPattern.length()),
-    		             new PlagiarismFragment(tmpText, textStart, textStart+tmpText.length(), tmpText.length()));
-    		}
-    		textStart+=tmpText.length();
+				String[] splitedPattern = prepareText(pattern);
+				for (String test : splitedPattern) {
+					if (textSentence.toLowerCase().contains(test.toLowerCase())) {
+						repeatedWords++;
+					}
+				}
+				repeatedWords -= 1;
+
+				if (repeatedWords / sentenceWordsCount > ServerProperties.MIN_WORD_SIMILARITY_PERCENTAGE / (double) 100) {
+					map.put(new PlagiarismFragment(tmpPattern, patternStart, patternStart + tmpPattern.length(), tmpPattern.length()),
+							new PlagiarismFragment(tmpText, textStart, textStart + tmpText.length(), tmpText.length()));
+				}
+			}
+			textStart += tmpText.length();
         }
         return map;
-    }
+	}
+
+	private String[] prepareText(String textSentence) {
+		String result;
+		result = filterLatex(textSentence);
+		result = clearText(result);
+		return result.split("\\s+");
+	}
+
+	private String filterLatex(String result) {
+		for (String regex: latexReplacements) {
+			result = result.replaceAll(regex, "");
+		}
+		return result;
+	}
+
+	private String clearText(String textSentence) {
+		textSentence.replaceAll("[\\{}\\(\\)\\-\\+\\.\\^:,]","");
+		textSentence = textSentence.replaceAll("\\b[\\w']{1,2}\\b", "");
+		textSentence = textSentence.replaceAll("\\s{2,}", " ");
+		return textSentence;
+	}
 
 }
